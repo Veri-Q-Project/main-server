f4a853d35df76336fc2d5a6f7ff7d84387e2d9ea
parkgogeun <vlg605@kyonggi.ac.kr>
2026-03-13 13:46:10 +0900

    QR 디코드 Api 개발 및 Redis 세팅

---
+++ src/main/java/com/veriq/veriqbe3/VeriQBe3Application.java
@@ -0,0 +1,13 @@
+package com.veriq.veriqbe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeriQBe3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeriQBe3Application.class, args);
+    }
+
+}
