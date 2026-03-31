347d034ad26b975c12f6f9aa2b0c55bb8b4d218c
sangmin1571 <ppassa1571@naver.com>
2026-03-31 15:58:53 +0900

    [refactor-KD-12]

---
+++ src/test/java/com/veriq/veriqgateway/VeriQGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veriq.veriqgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeriQGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
