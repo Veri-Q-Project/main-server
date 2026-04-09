6553bba941b5906f003aed18584747629c77dce5
sangmin1571 <ppassa1571@naver.com>
2026-04-09 23:25:49 +0900

    refactor-fix/develop

--- src/main/java/com/veriq/veriqgateway/controller/GatewayController.java
+++ src/main/java/com/veriq/veriqgateway/controller/GatewayController.java
@@ -1,6 +1,7 @@
 package com.veriq.veriqgateway.controller;
 import com.veriq.veriqgateway.dto.ScanResponse;
 import com.veriq.veriqgateway.service.SecurityService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Profile("be1")
 public class GatewayController {
     private final SecurityService securityService;
     private final RestTemplate restTemplate;
