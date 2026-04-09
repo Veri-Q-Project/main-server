3f27b101b808472ce3104b8e1c2e23d30d1d84b5
parkgogeun <vlg605@kyonggi.ac.kr>
2026-04-09 10:12:00 +0900

    불필요한 주석 수정

--- src/main/java/com/veriq/veriqbe3/service/ProcessQrScan.java
+++ src/main/java/com/veriq/veriqbe3/service/ProcessQrScan.java
@@ -17,7 +17,7 @@ public class ProcessQrScan {
     private final SchemeClassifier schemeClassifier;
     private final ScanHistoryRepository scanHistoryRepository;
 
-    @Transactional // DB 저장 중 에러 발생 시 모든 작업을 롤백하기 위함
+    @Transactional // DB 저장 중 에러 발생 시 롤백하기 위함
     public QrScanResponse process(MultipartFile image, String guest_uuid) throws Exception {
 
         String url = qrDecoder.decode(image);         //이미지 디코드
@@ -34,7 +34,6 @@ public class ProcessQrScan {
 
         String status = "COMPLETED";
 
-        //WEB이나 단축 URL이면
         if (result.type() == SchemeType.WEB || result.type() == SchemeType.SHORT_URL) {
             return QrScanResponse.builder()
                     .guestUuid(guest_uuid)
@@ -44,7 +43,7 @@ public class ProcessQrScan {
                     .build();
         }
         return QrScanResponse.builder()
-                .guestUuid(guest_uuid)          // FE가 보낸 식별자
+                .guestUuid(guest_uuid)
                 .schemeType(result.type())
                 .typeInfo(result.typeInfo())
                 .status(status)
