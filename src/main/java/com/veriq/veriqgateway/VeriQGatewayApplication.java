01cdd1b2bc25f4db4a6df42338c83e6e6faadfc4
sangmin1571 <ppassa1571@naver.com>
2026-04-11 16:17:16 +0900

    test:be1->be3 이미지 전송

--- src/main/java/com/veriq/veriqgateway/VeriQGatewayApplication.java
+++ src/main/java/com/veriq/veriqgateway/VeriQGatewayApplication.java
@@ -16,12 +16,6 @@ public class VeriQGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(VeriQGatewayApplication.class, args);
     }
-    /**
-     * 고근 님(BE 3) 서버와 통신할 때 사용할 RestTemplate 빈을 등록
-     */
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+
 
 }
