60a9b9330188f2791034e1b498d62efe2dca4b9b
sangmin1571 <ppassa1571@naver.com>
2026-04-30 03:22:03 +0900

    fix-분석서버쪽 dto수정3

--- src/main/java/com/veriq/veriqbe3/dto/AnalysisResponse.java
+++ src/main/java/com/veriq/veriqbe3/dto/AnalysisResponse.java
@@ -11,6 +11,7 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AnalysisResponse(
         String analysisTime,
+        @JsonAlias({"original_url", "url", "originalUrl"})
         String originalUrl,
         HttpsInfo https,
         ShortUrlInfo shortUrl,
